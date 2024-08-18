14bf90dd2a0cae51abfaca4451c8d853712eec8f
Winnie Wei <104879662+NineWei@users.noreply.github.com>
2024-08-18 00:26:36 -0300

    Add files via upload

---
+++ Prova 2 Infinity Winnie.py
@@ -0,0 +1,12 @@
+#Prova2InfinitySchool 
+#FullStack
+#WinnieWei
+
+numero = int(input("Digite um número: "))
+
+if numero < 0 :
+    print(f"O número {numero} é negativo.")
+elif numero > 0 :
+    print(f"O número {numero} é positivo.")
+else :
+    print(f"O número {numero} é zero.")
